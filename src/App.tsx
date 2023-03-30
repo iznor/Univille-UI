b161c194c2b5b3d923880cf19a5edf55211da8e2
iZnoR <76132625+iznor@users.noreply.github.com>
2023-03-30 16:33:21 +0300

    init0.02-basework

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export const App = (): JSX.Element => {
-  return (<div>     UnivilleUi</div>)
-}
+  return <div>UnivilleUi</div>;
+};
 
-export default App
+export default App;
