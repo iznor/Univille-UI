b65a7d969672b216f6d554cd0dabec2e1aafeb3d
iZnoR <76132625+iznor@users.noreply.github.com>
2023-04-05 16:50:42 +0300

    add select + remove capabilities to the location selector

--- src/pages/GameWizard/Item/Item.tsx
+++ src/pages/GameWizard/Item/Item.tsx
@@ -1,18 +1,33 @@
 import { Box, Button, Paper } from '@mui/material';
 import { H2, P } from '../../../components';
-import React from 'react';
-
-export const Item = (props) => {
+import React, { useEffect, useState } from 'react';
+import { LocationItem } from '../consts';
+type ItemProps = {
+  item: LocationItem;
+  selectItem: (item: LocationItem) => void;
+  removeItem: (item: LocationItem) => void;
+};
+export const Item = (props: ItemProps) => {
+  const { item, selectItem, removeItem } = props;
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [buttonText, setButtonText] = useState<string>('Add this location');
+  useEffect(() => {}, [isSelected]);
+  const handleButtonClick = () => {
+    isSelected ? removeItem(item) : selectItem(item);
+    setIsSelected(!isSelected);
+  };
   return (
     <Box className="location-selector-wrapper">
       <Paper className="paper">
-        <img src={props.item.objectPhotoUrl} className="item-photo-box" />
+        <img src={item.objectPhotoUrl} className="item-photo-box" />
         <Box className="item-description-box">
-          <H2>{props.item.name}</H2>
-          <P>{props.item.description}</P>
-          <Button className="CheckButton">Add this location</Button>
+          <H2>{item.name}</H2>
+          <P>{item.description}</P>
+          <Button className="CheckButton" onClick={handleButtonClick}>
+            {isSelected ? 'Remove from list' : 'Add this location'}
+          </Button>
         </Box>
-        <img src={props.item.mapPhotoUrl} className="item-photo-box" />
+        <img src={item.mapPhotoUrl} className="item-photo-box" />
       </Paper>
     </Box>
   );
