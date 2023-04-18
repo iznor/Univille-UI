8dae500973398a60a15870652e7a16cac410555a
ofer221 <ofer2212@gmail.com>
2023-04-18 16:46:26 +0300

    added redux and server api functionality

--- src/store/hooks/useData.ts
+++ src/store/hooks/useData.ts
@@ -1,23 +1,20 @@
-import {useCallback} from 'react';
-import {useAppDispatch, useAppSelector} from "../store";
-import {dataActions} from "../actions";
-import {IDataState} from "../reducers";
+import { useCallback } from 'react';
+import { useAppDispatch, useAppSelector } from '../store';
+import { dataActions } from '../actions';
+import { IDataState } from '../reducers';
 
 export function useData() {
-    const dispatch = useAppDispatch();
-    const values:IDataState = useAppSelector(store => store.data);
+  const dispatch = useAppDispatch();
+  const values: IDataState = useAppSelector((store) => store.data);
 
-    const setData = (dataState: Partial<IDataState>) => {
-        dispatch(dataActions.setData(dataState))
-    }
+  const setData = (dataState: Partial<IDataState>) => {
+    dispatch(dataActions.setData(dataState));
+  };
 
-
-
-    return {
-        dataState: values,
-        dataActions: {
-            setData: useCallback(setData, []),
-        }
-    }
-
+  return {
+    dataState: values,
+    dataActions: {
+      setData: useCallback(setData, []),
+    },
+  };
 }
