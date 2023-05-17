2a84cd9bd30477eb567e384d1d5697cbf07ee054
ofer221 <ofer2212@gmail.com>
2023-05-18 00:41:31 +0300

    install code quality tools

---
+++ global.d.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+
+declare module 'express' {
+  interface Request<P, ResBody, ReqBody, ReqQuery, Locals> {
+    body: ReqBody;
+    params: ReqQuery;
+  }
+
+  interface Response<
+    ResBody = any,
+    Locals extends Record<string, any> = Record<string, any>,
+    StatusCode extends number = number
+  > {
+    status(code: StatusCode): this;
+
+    json: (res: ResBody) => any;
+  }
+}
+
+declare global {
+  type IControlFn<Tbody = any, Tparams = any> = (
+    req: Request<never, never, Tbody, Tparams>,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>;
+
+  interface IGameMeta {
+    name: string;
+    duration: number;
+    groupCount: number;
+    colors: string[];
+    code: string[];
+    startTime: number;
+  }
+  interface IGameParams {
+    teacherId: string;
+    className: string;
+    missions: Partial<IMission>[];
+    metadata: Partial<IGameMeta>;
+  }
+
+  export interface IMission {
+    name: string;
+    description: string;
+    hint: { text: string; image?: string; title?: string };
+    target: { objectTag: string; image?: string; title?: string };
+    image: string;
+    score: number;
+  }
+}
+
+export {};
